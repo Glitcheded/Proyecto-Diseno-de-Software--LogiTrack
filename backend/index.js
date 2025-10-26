ed648206d72beb7cf53ffb319440f889640bc43d
danielviquezsolano <daviquez@estudiantec.cr>
2025-10-25 21:54:04 -0600

    Configuración del servidor backend
    Se inicializa servidor, se instalaron dependencias del backend, conexión a base de datos, se creó un servidor básico y se probó exitosamente.
    Adicionalmente se movió y editó el archivo .gitignore para compartir con la carpeta backend

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Importar futuras rutas
+// import authRoutes from './routes/auth.js';
+// import taskRoutes from './routes/tasks.js';
+// import projectRoutes from './routes/projects.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001; // El frontend corre en 5173, el backend en 3001
+
+// Middlewares
+app.use(cors()); // Permite peticiones desde el frontend
+app.use(express.json()); // Permite a Express entender JSON en el body
+
+// Rutas de tu API
+app.get('/api', (req, res) => {
+  res.send('API de LogiTrack funciona');
+});
+
+// Conectar las rutas
+// app.use('/api/auth', authRoutes);
+// app.use('/api/tasks', taskRoutes);
+// app.use('/api/projects', projectRoutes);
+
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
